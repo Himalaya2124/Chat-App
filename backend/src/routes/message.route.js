e5dda0cd70a5bfc9b691c648a533978af5e80297
Himalaya <himalayajindal@gmail.com>
2025-07-02 13:09:44 +0530

    Final Draft

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -5,7 +5,7 @@ import { sendMessage, getMessages, getUsersForSidebar } from "../controllers/mes
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar)
-// router.get("/:id", protectRoute, getMessages)
-// router.post("/send/:id", protectRoute, sendMessage)
+router.get("/:id", protectRoute, getMessages)
+router.post("/send/:id", protectRoute, sendMessage)
 
 export default router;
